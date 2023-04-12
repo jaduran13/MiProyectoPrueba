24163a511257bdb0db52586bc12419748d81cc2a
jadg13 <jaduran@doc.uca.edu.ni>
2023-04-12 08:00:56 -0600

    se manda a llamar el archivo menu

--- principal.cpp
+++ principal.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "MisArchivos\\funciones.cpp"
-
+#include "MisArchivos\\menu.cpp"
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     cout << "\tMi proyecto - Prueba" << endl;
+    bienvenida();
+
     return 0;
 }
