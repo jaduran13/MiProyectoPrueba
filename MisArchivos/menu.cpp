cc0e2be9112d89e31cfedfdce9626c0cf5425d9e
jadg13 <jaduran@doc.uca.edu.ni>
2023-04-12 07:43:52 -0600

    se agrego el archivo menu

---
+++ MisArchivos/menu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void bienvenida();
+
+void bienvenida()
+{
+    cout << "Universidad Centroamericana" << endl;
+    cout << "Somos UCA" << endl;
+    cout << "Ingenieria en Sistemas de Informacion" << endl;
+}
